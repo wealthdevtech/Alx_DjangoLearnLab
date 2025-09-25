cc035813b33294c90ed3058b5109745feb47df15
wealthdevtech <wealthydaniel56@gmail.com>
2025-09-25 15:32:16 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,4 +1,6 @@
 Query all books by a specific author.
+"Author.objects.get(name=author_name)"
+"objects.filter(author=author)"
 List all books in a library.
 "Library.objects.get(name=library_name)"
 "books.all()"
