ae234f7aeaa28cd79ab21e9f3151ef8c3da72233
wealthdevtech <wealthydaniel56@gmail.com>
2025-09-26 22:37:35 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, loader
 from django.http import HttpResponse
-from .models import Book, Library
+from .models import Library
 from django.views.generic import ListView
 
 # Create your views here.
@@ -14,7 +14,7 @@ def list_books(request):
 
 class library_detailView(ListView):
     model = ListView
-    template_name = 'templates/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
      
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
