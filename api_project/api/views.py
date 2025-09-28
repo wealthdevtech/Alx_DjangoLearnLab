381c4ad4d5a093e8b0b2a4eba17a03fc5dc7e118
wealthdevtech <wealthydaniel56@gmail.com>
2025-09-28 20:08:25 +0200

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics 
 from .models import MyModel
-from .serializers import MyModelSerializer
+from .serializers import BookSerializer
 from django.shortcuts import render
 
 # Create your views here.
